a4ca97a4e8f1a576591584daa4748ca08f0b5e6b
creategamer <creategamer10@gmail.com>
2024-08-13 16:29:56 +0530

    Applied changes from previous repo

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':"http://localhost:8200",
+  server: {
+    proxy: {
+      '/api': "https://foodiezonebackend.onrender.com/",
     },
   },
   plugins: [react()],
