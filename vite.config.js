321f2b17a9ffd4a6d42811156eb99f29e6bead3d
creategamer <creategamer10@gmail.com>
2024-08-13 21:21:18 +0530

    save this page

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': "https://foodiezonebackend.onrender.com/",
+      '/api': "https://foodiezonebackend.onrender.com",
     },
   },
   plugins: [react()],
